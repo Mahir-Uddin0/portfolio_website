a48a56cc0ed57c26227c42fd2f510e4d29895789
Mahir-Uddin0 <mahir.uddin.0@gmail.com>
2026-01-21 14:29:32 +0600

    updated projects section

--- pages/projects.js
+++ pages/projects.js
@@ -24,25 +24,30 @@ export default function Projects() {
 
 
           {/* 1. Project Card */}
-          <div className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
-            
+          <div className="bg-gray-100 dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
+
             <div>
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                LLM Fine-Tuning with RAG for  Investment Guidance
+                LLM Fine-Tuning with RAG for Investment Guidance
               </h3>
 
               <p className="mt-3 text-sm text-justify text-gray-600 dark:text-gray-400 leading-relaxed">
-                Developed a domain-specific investing assistant by fine-tuning the LLaMA 3.2 (3B) model on finance and investment datasets. Integrated a Retrieval-Augmented Generation (RAG) pipeline that dynamically pulls information from investment books, company annual reports, and relevant web articles to provide context-aware, data-driven investment guidance.
+                Developed a domain-specific investing assistant by fine-tuning the LLaMA 3.2 (3B) model on
+                finance and investment datasets. Integrated a Retrieval-Augmented Generation (RAG) pipeline
+                that dynamically pulls information from investment books, company annual reports, and
+                relevant web articles to provide context-aware, data-driven investment guidance.
               </p>
 
-              <div className="mt-4 flex flex-wrap gap-2">
-                <span className="skill-pill">LLM</span>
-                <span className="skill-pill">LangChain</span>
-                <span className="skill-pill">Vector Database</span>
-              </div>
             </div>
 
             <div className="mt-6">
+              <div className="mt-4 flex flex-wrap gap-2">
+                <span className="skill-pill">LLM</span>
+                <span className="skill-pill">LangChain</span>
+                <span className="skill-pill">Vector DB</span>
+                <span className="skill-pill">RAG</span>
+              </div>
+              <br></br>
               <a
                 href="https://github.com/Mahir-Uddin0/llm-finetuning-rag"
                 target="_blank"
@@ -55,64 +60,31 @@ export default function Projects() {
             </div>
           </div>
 
-          {/* 2. Project Card */}
-          <div className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
-            
-            <div>
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                Smart EV Charging Scheduler for Self-Driving Vehicles
-              </h3>
+          {/* 3. Project Card */}
+          <div className="bg-gray-100  dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
 
-              <p className="mt-3 text-sm text-gray-600 dark:text-gray-400 leading-relaxed">
-                Designed an automated scheduler for self-driving electric vehicles that optimizes charging assignments based on various data
-                such as battery levels, station availability, and travel distances. Implemented with MiniZinc and constraint programming to ensure efficient, 
-                conflict-free scheduling while minimizing total travel distance.
-              </p>
-
-              <div className="mt-4 flex flex-wrap gap-2">
-                <span className="skill-pill">MiniZinc</span>
-                <span className="skill-pill">CSP</span>
-                <span className="skill-pill">Optimization</span>
-              </div>
-            </div>
-
-            <div className="mt-6">
-              <a
-                href="https://github.com/Mahir-Uddin0/EV-charging-scheduler"
-                target="_blank"
-                rel="noreferrer"
-                className="inline-flex items-center gap-2 text-sm font-medium text-indigo-600 dark:text-indigo-400 hover:underline"
-              >
-                <FaGithub className="text-lg" />
-                View Code
-              </a>
-            </div>
-          </div>
-
-          {/* 3. project card */}
-          <div className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
-            
             <div>
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                 Density-Aware Dynamic Cluster Splitting Algorithm
               </h3>
 
-              <p className="mt-3 text-sm text-gray-600 dark:text-gray-400 leading-relaxed">
-                Designed a density-aware post-processing algorithm that automatically detects and 
-                splits erroneously merged k-means clusters. The method recursively refines clusters 
-                using spatial density analysis, removing dependency on correct k initialization and 
+              <p className="mt-3 text-sm text-justify text-gray-600 dark:text-gray-400 leading-relaxed">
+                Designed a density-aware post-processing algorithm that automatically detects and
+                splits erroneously merged k-means clusters. The method recursively refines clusters
+                using spatial density analysis, removing dependency on correct k initialization and
                 improving robustness for complex, overlapping data distributions.              </p>
 
-              {/* Tech Stack */}
-              <div className="mt-4 flex flex-wrap gap-2">
-                <span className="skill-pill">PyTorch</span>
-                <span className="skill-pill">CNN</span>
-                <span className="skill-pill">Computer Vision</span>
-              </div>
             </div>
 
             {/* GitHub */}
             <div className="mt-6">
+              {/* Tech Stack */}
+              <div className="mt-4 flex flex-wrap gap-2">
+                <span className="skill-pill">Numpy</span>
+                <span className="skill-pill">Pandas</span>
+                <span className="skill-pill">Scikit-Learn</span>
+              </div>
+              <br></br>
               <a
                 href="https://github.com/Mahir-Uddin0/density-aware-cluster-splitting"
                 target="_blank"
@@ -125,28 +97,103 @@ export default function Projects() {
             </div>
           </div>
 
+          {/* 3. Project Card */}
+          <div className="bg-gray-100  dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
+
+            <div>
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Smart EV Charging Scheduler for Self-Driving Vehicles
+              </h3>
+
+              <p className="mt-3 text-sm text-justify text-gray-600 dark:text-gray-400 leading-relaxed">
+                Designed an automated scheduler for self-driving electric vehicles that optimizes charging assignments based on various data
+                such as battery levels, station availability, and travel distances. Implemented with MiniZinc and constraint programming to ensure efficient,
+                conflict-free scheduling while minimizing total travel distance.
+              </p>
+
+            </div>
+
+            <div className="mt-6">
+              <div className="mt-4 flex flex-wrap gap-2">
+                <span className="skill-pill">MiniZinc</span>
+                <span className="skill-pill">CSP</span>
+                <span className="skill-pill">Optimization</span>
+              </div>
+              <br></br>
+              <a
+                href="https://github.com/Mahir-Uddin0/EV-charging-scheduler"
+                target="_blank"
+                rel="noreferrer"
+                className="inline-flex items-center gap-2 text-sm font-medium text-indigo-600 dark:text-indigo-400 hover:underline"
+              >
+                <FaGithub className="text-lg" />
+                View Code
+              </a>
+            </div>
+          </div>
+
           {/* 4. Project Card */}
-          <div className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
-            
+          <div className="bg-gray-100  dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
+
+            <div>
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Stock Market Trend Prediction using ANFIS
+              </h3>
+
+              <p className="mt-3 text-sm text-justify text-gray-600 dark:text-gray-400 leading-relaxed">
+                This project applies an Adaptive Neuro-Fuzzy Inference System (ANFIS) to predict short-term
+                stock market trends using technical indicators such as RSI, MACD, and Volume Ratios. By
+                integrating fuzzy logic with neural network learning, the model automatically learns fuzzy
+                rules that capture non-linear patterns in stock price movements.
+              </p>
+
+
+            <div className="mt-6">
+              <div className="mt-4 flex flex-wrap gap-2">
+                <span className="skill-pill">ANFIS</span>
+                <span className="skill-pill">PyTorch</span>
+                <span className="skill-pill">Scikit-learn</span>
+              </div>
+            </div>
+            <br></br>
+              <a
+                href="https://github.com/Mahir-Uddin0/stock-trend-identification-using-ANFIS"
+                target="_blank"
+                rel="noreferrer"
+                className="inline-flex items-center gap-2 text-sm font-medium text-indigo-600 dark:text-indigo-400 hover:underline"
+              >
+                <FaGithub className="text-lg" />
+                View Code
+              </a>
+            </div>
+          </div>
+
+          {/* 5. Project Card */}
+          <div className="bg-gray-100  dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
+
             <div>
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                 Uber Fare Prediction with Data Cleaning & EDA
               </h3>
 
-              <p className="mt-3 text-sm text-gray-600 dark:text-gray-400 leading-relaxed">
-                Performed rigorous data cleaning and exploratory data analysis to identify key factors influencing Uber fares. Built and refined regression models with regularization. Validated insights and model performance using error analysis and statistical tests.
+              <p className="mt-3 text-sm text-justify text-gray-600 dark:text-gray-400 leading-relaxed">
+                Performed rigorous data cleaning and exploratory data analysis to identify key factors
+                influencing Uber fares. Built and refined regression models with regularization. Validated
+                insights and model performance using error analysis and statistical tests.
               </p>
 
-              {/* Tech Stack */}
-              <div className="mt-4 flex flex-wrap gap-2">
-                <span className="skill-pill">PyTorch</span>
-                <span className="skill-pill">CNN</span>
-                <span className="skill-pill">Computer Vision</span>
-              </div>
             </div>
 
             {/* GitHub */}
             <div className="mt-6">
+              {/* Tech Stack */}
+              <div className="mt-4 flex flex-wrap gap-2">
+                <span className="skill-pill">EDA</span>
+                <span className="skill-pill">Statistics</span>
+                <span className="skill-pill">Pandas</span>
+                <span className="skill-pill">Matplotlib</span>
+              </div>
+              <br></br>
               <a
                 href="https://github.com/Mahir-Uddin0/Uber-Fare-Prediction"
                 target="_blank"
@@ -160,63 +207,30 @@ export default function Projects() {
           </div>
 
 
-          {/* 5. Project Card */}
-          <div className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
-            
-            <div>
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                Stock Market Trend Prediction using ANFIS
-              </h3>
-
-              <p className="mt-3 text-sm text-gray-600 dark:text-gray-400 leading-relaxed">
-                This project applies an Adaptive Neuro-Fuzzy Inference System (ANFIS) to predict short-term 
-                stock market trends using technical indicators such as RSI, MACD, and Volume Ratios. By 
-                integrating fuzzy logic with neural network learning, the model automatically learns fuzzy 
-                rules that capture non-linear patterns in stock price movements.
-              </p>
-
-              <div className="mt-4 flex flex-wrap gap-2">
-                <span className="skill-pill">ANFIS</span>
-                <span className="skill-pill">PyTorch</span>
-                <span className="skill-pill">Scikit-learn</span>
-              </div>
-            </div>
-
-            <div className="mt-6">
-              <a
-                href="https://github.com/Mahir-Uddin0/stock-trend-identification-using-ANFIS"
-                target="_blank"
-                rel="noreferrer"
-                className="inline-flex items-center gap-2 text-sm font-medium text-indigo-600 dark:text-indigo-400 hover:underline"
-              >
-                <FaGithub className="text-lg" />
-                View Code
-              </a>
-            </div>
-          </div>
 
           {/* 6. Project Card */}
-          <div className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
-            
+          <div className="bg-gray-100  dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
+
             <div>
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                 Technical Analysis and Predictive Insights on Stock Returns
               </h3>
 
-              <p className="mt-3 text-sm text-gray-600 dark:text-gray-400 leading-relaxed">
-                Developed a Python-based system to compute key technical indicators (SMA, EMA, RSI, MACD, Volume) 
-                and analyze stock return distributions under custom conditions. Enables data-driven insights for 
+              <p className="mt-3 text-sm text-justify text-gray-600 dark:text-gray-400 leading-relaxed">
+                Developed a Python-based system to compute key technical indicators (SMA, EMA, RSI, MACD, Volume)
+                and analyze stock return distributions under custom conditions. Enables data-driven insights for
                 identifying high-probability bullish scenarios for stock price.
               </p>
 
+            </div>
+
+            <div className="mt-6">
               <div className="mt-4 flex flex-wrap gap-2">
                 <span className="skill-pill">Scikit-learn</span>
                 <span className="skill-pill">Matplotlib</span>
                 <span className="skill-pill">TA</span>
               </div>
-            </div>
-
-            <div className="mt-6">
+              <br></br>
               <a
                 href="https://github.com/Mahir-Uddin0/stock-return-analysis/tree/main"
                 target="_blank"
@@ -230,27 +244,28 @@ export default function Projects() {
           </div>
 
           {/* 7. Project Card */}
-          <div className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
-            
+          <div className="bg-gray-100  dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm hover:shadow-lg transition p-6 flex flex-col justify-between">
+
             <div>
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                 Intelligent Crime Solver using Prolog
               </h3>
 
-              <p className="mt-3 text-sm text-gray-600 dark:text-gray-400 leading-relaxed">
-                Built a logic-based AI system using Knowledge Representation & Reasoning to investigate 
-                complex murder scenarios. The model evaluates motives, evidence, contradictions, and 
-                cover-ups to identify the killer(s), demonstrating advanced logical deduction, structured 
+              <p className="mt-3 text-sm text-justify text-gray-600 dark:text-gray-400 leading-relaxed">
+                Built a logic-based AI system using Knowledge Representation & Reasoning to investigate
+                complex murder scenarios. The model evaluates motives, evidence, contradictions, and
+                cover-ups to identify the killer(s), demonstrating advanced logical deduction, structured
                 reasoning, and AI-driven problem solving.
               </p>
 
+            </div>
+
+            <div className="mt-6">
               <div className="mt-4 flex flex-wrap gap-2">
                 <span className="skill-pill">Prolog</span>
                 <span className="skill-pill">Expert Systems</span>
               </div>
-            </div>
-
-            <div className="mt-6">
+              <br></br>
               <a
                 href="https://github.com/Mahir-Uddin0/prolog-knowledge-based-crime-solver/tree/main"
                 target="_blank"
@@ -265,7 +280,7 @@ export default function Projects() {
 
         </div>
 
-                
+
       </section>
     </main>
   );
